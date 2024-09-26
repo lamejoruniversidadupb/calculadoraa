9aa28e71b548ee9302cfb7fde26dcce9e5523ce1
rAfA-jp <regstar159@gmail.com>
2024-09-26 13:38:27 -0500

    multiplicacion

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,1 +1,18 @@
+#include <iostream>
 
+using namespace std; 
+
+int main(){
+int a,b,resultado;
+cout<<"ingrese el primer numero: ";
+cin>> a;
+cout<<"ingrese el segundo numero: ";
+cin>> b;
+
+resultado = a * b;
+
+cout << "El resultado de "<< a <<"*"<< b <<"es: "<< resultado << endl;
+
+return 0;
+
+}
