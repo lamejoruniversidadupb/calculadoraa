c35e6be9d5614fa494c76643217649e2943084c3
GabrielSastoque <sastoquegabriel95@gmail.com>
2024-09-26 13:44:09 -0500

    Submit DivisionOpe

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+using namespace std;
+
+int main(){
+int a, b, xResultado;
+xResultado=0;
+cout<<"Ingresa un número -a- e";
+cout<<"Ingresa el denominador -b-\n";
+cin>>a,b;
+
+if(a==0 && b==0){
+	cout<<"Error";
+}else{
+	xResultado= a/b;
+    }
+
+ return 0;   
+}
