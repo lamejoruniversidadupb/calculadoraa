05f0d2b9307906d2fb218776f31fcbffecffc9de
ryuzakireal <apartaro@gmail.com>
2024-09-26 13:47:38 -0500

    se añadio resta

--- calculadora.cpp
+++ calculadora.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
+ using namespace std;
 
+ int main (){
+    // Declaración de variables
+    float numero1, numero2, resultado;
+
+    // Solicitar los números al usuario
+    cout << "ingresa el primer número";
+    cin >> numero1;
+    cout << "ingresa el segundo número";
+    cin >> numero2;
+     
+     // Realizar la resta
+    resultado = numero1 - numero2;
+
+    // Mostrar el resultado
+    cout << "La resta de" << numero1 << "-" << numero2 "es;" << resultado << endl;
+    return 0;
+ }
+ 
